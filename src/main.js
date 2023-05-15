08024555fc8c10e4272cf57be3a9ca595dcf24a0
janbreitenbachoha <106101095+janbreitenbachoha@users.noreply.github.com>
2023-05-15 08:22:27 +0200

    dfdfsfsd

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import Dialog from "primevue/dialog";
 import ToastService from 'primevue/toastservice';
 
 
-
+import '@fortawesome/fontawesome-free/css/all.css'
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
--- src/views/ChatTest.vue
+++ src/views/ChatTest.vue
@@ -1,35 +1,55 @@
 <template>
-	<div>
-		<div v-for="(message, index) in messages" :key="index">
-			<div v-if="message.type === 'text'">
-				<p v-html="formatText(message.text)"></p>
-				<div v-if="message.image" class="message-image">
-					<img
-						:src="message.image"
-						@click="showImage(message.image)"
-						class="thumbnail"
-					/>
+	<div class="chat-container">
+		<div class="message-list" ref="messageList">
+			<div
+				v-for="message in messages"
+				:key="message.timestamp"
+				class="message-container"
+			>
+				<div class="message" :class="{ 'from-me': message.fromMe }">
+					<div v-if="message.image" class="message-image">
+						<img
+							:src="message.image"
+							class="thumbnail"
+							@click="showImage(message.image)"
+						/>
+					</div>
+					<div
+						v-if="showFullImage === message.image"
+						class="full-image-overlay"
+					>
+						<img :src="showFullImage" @click="hideImage" class="full-image" />
+					</div>
+					<div class="message-content">
+						<div class="message-text">{{ formatText(message.text) }}</div>
+					</div>
 				</div>
-			</div>
-			<div v-else-if="message.type === 'image'">
-				<div class="message-image">
-					<img
-						:src="message.image"
-						@click="showImage(message.image)"
-						class="thumbnail"
-					/>
-				</div>
-				<p v-html="formatText(message.text)"></p>
 			</div>
 		</div>
-		<form @submit.prevent="sendMessage">
-			<textarea
-				v-model="newMessage"
-				placeholder="Type your message"
-				rows="3"
-			></textarea>
-			<input type="file" @change="onFileChange" />
-			<button>Send</button>
+
+		<form class="input-container" @submit.prevent="sendMessage">
+			<div>
+				<label for="image-upload" class="image-button">
+					<i class="fas fa-image"></i>
+				</label>
+				<input
+					id="image-upload"
+					type="file"
+					accept="image/*"
+					style="display: none"
+					@change="onImageSelected"
+				/>
+			</div>
+			<div class="text-container">
+				<textarea
+					v-model="newMessage"
+					class="text-input"
+					placeholder="Nachricht eingeben..."
+				></textarea>
+				<button type="button" class="send-button" @click="sendMessage">
+					Send
+				</button>
+			</div>
 		</form>
 	</div>
 </template>
@@ -53,6 +73,7 @@
 						text: this.newMessage,
 						image: URL.createObjectURL(this.newImage),
 						timestamp: Date.now(),
+						fromMe: true,
 					};
 					this.newImage = null;
 				} else {
@@ -61,13 +82,17 @@
 						text: this.newMessage,
 						image: null,
 						timestamp: Date.now(),
+						fromMe: true,
 					};
 				}
 				this.messages.push(message);
 				this.newMessage = "";
 			},
-			onFileChange(event) {
+			onImageSelected(event) {
 				this.newImage = event.target.files[0];
+			},
+			openImageDialog() {
+				this.$refs.imageInput.click();
 			},
 			showImage(imageUrl) {
 				this.showFullImage = imageUrl;
@@ -83,32 +108,111 @@
 </script>
 
 <style scoped>
-	.thumbnail {
-		max-width: 100px;
-		max-height: 100px;
-		cursor: pointer;
+	.chat-container {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
 	}
 
+	.message-list {
+		flex-grow: 1;
+		overflow-y: auto;
+		padding: 10px;
+	}
+
+	.message-container {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 10px;
+	}
+
+	.message {
+		display: flex;
+		align-items: center;
+	}
+
+	.message-image {
+		margin-right: 10px;
+	}
+
+	.thumbnail {
+		max-width: 200px;
+		max-height: 200px;
+	}
+
+	.message-content {
+		background-color: #eee;
+		padding: 10px;
+		border-radius: 10px;
+	}
+
+	.message-text {
+		white-space: pre-wrap;
+	}
+
+	.message-timestamp {
+		font-size: 12px;
+		color: gray;
+		margin-top: 5px;
+	}
+
+	.input-container {
+		display: flex;
+		align-items: center;
+		padding: 10px;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-color: #fff;
+		z-index: 999;
+	}
+
+	.image-button {
+		border: none;
+		background-color: transparent;
+		font-size: 20px;
+		color: gray;
+	}
+
+	.text-container {
+		flex-grow: 1;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-left: 10px;
+	}
+
+	.text-input {
+		flex-grow: 1;
+		border: none;
+		resize: none;
+		font-size: 16px;
+		padding: 10px;
+		border-radius: 10px;
+	}
+
+	.send-button {
+		border: none;
+		background-color: transparent;
+		font-size: 20px;
+		color: #007aff;
+	}
 	.full-image-overlay {
 		position: fixed;
 		top: 0;
 		left: 0;
-		right: 0;
-		bottom: 0;
-		background-color: rgba(0, 0, 0, 0.8);
+		width: 100%;
+		height: 100%;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		background-color: rgba(0, 0, 0, 0.5);
 		z-index: 999;
 	}
 
 	.full-image {
-		max-width: 90vw;
-		max-height: 90vh;
-	}
-
-	.message-image {
-		display: inline-block;
-		margin-right: 10px;
+		max-width: 90%;
+		max-height: 90%;
 	}
 </style>
